5c38af01d6db78479870fa61de04c35329fbfe77
GriffonAnton <griffonanton@gmail.com>
2023-01-22 22:16:31 +0100

    fix: put the router in separate component feat: added header and footer component directories and files

---
+++ src/components/MainRouter/index.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import FicheLogement from './pages/FicheLogement';
+import APropos from './pages/APropos';
+import Error from './pages/Error';
+
+function MainRouter() {
+    return (
+        <Router>
+            <Route exact path="/">
+                <Home/>
+            </Route>
+            <Route path="/fiche-logement/:logementId">
+                <FicheLogement/>
+            </Route>
+            <Route path="/a-propos">
+                <APropos/>
+            </Route>
+            <Route>
+                <Error/>
+            </Route>
+        </Router>
+    );
+}
+
+export default MainRouter;
