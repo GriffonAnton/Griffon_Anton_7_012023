849cbd1e2f58fe9c8d875c61e4f89db8c9e2911d
GriffonAnton <griffonanton@gmail.com>
2023-02-13 00:07:23 +0100

    feat: created fiche logement page

--- src/pages/FicheLogement/index.jsx
+++ src/pages/FicheLogement/index.jsx
@@ -1,7 +1,11 @@
 
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import React, { Component }  from 'react';
 import { useParams } from 'react-router-dom';
 import Carrousel from '../../components/Carrousel';
+import Collapse from '../../components/Collapse';
 import Logements from '../../data/logements.json';
 
 function FicheLogement() {
@@ -9,15 +13,48 @@ function FicheLogement() {
     const idLogement = useParams().logementId;
     const logementsList = Logements;
     const infoLogement = logementsList.find(logement => logement.id === idLogement);
-    console.log('infoLogement')
-    console.log(infoLogement);
-    const picturesList = infoLogement.pictures;
-    console.log('picturesList')
-    console.log(picturesList);
+    const ratingStars = () => {
+        let stars = [];
+        for (let i = 1; i <= 5; i++){
+            if (+infoLogement.rating >= i) {
+                stars.push(<FontAwesomeIcon className='rating__star rating__star--color' icon={solid('star')}/>);
+            } else {
+                stars.push(<FontAwesomeIcon className='rating__star rating__star--grey' icon={solid('star')}/>);
+            }
+        }
+        return stars;
+    }
+    console.log(ratingStars)
     
     return (
         <section className='fiche_logement'>
-            <Carrousel picturesList={picturesList}/>
+            <Carrousel picturesList={infoLogement.pictures} />
+            <div className='fiche_logement__header'>
+                <div className='fiche_logement__header__left'>
+                    <h1>{infoLogement.title}</h1>
+                    <p>{infoLogement.location}</p>
+                    <div className='tags'>
+                        {infoLogement.tags.map(tag => <div className='tags__tag'>{tag}</div>)}
+                    </div>
+                </div>
+                <div className='fiche_logement__header__right'>
+                    <div className='host'>
+                        <p>{infoLogement.host.name}</p>
+                        <img  src={infoLogement.host.picture} alt={'photo de profil de l\'hôte'}/>
+                    </div>
+                    <div className='rating'>
+                        {ratingStars()}
+                    </div>
+                </div>
+            </div>
+            <div className='fiche_logement__collapses'>
+                <div>
+                    <Collapse title='Description' text={infoLogement.description} />
+                </div>
+                <div>
+                    <Collapse title='Equipements' text={infoLogement.equipments.map(equipement => equipement + ('\n'))}/>
+                </div>
+            </div>
         </section>
     );
 };
